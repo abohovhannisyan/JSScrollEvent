ff92ce6225817cbfb61b2d873f5a572fd0a7a951
abohovhannisyan <47552500+abohovhannisyan@users.noreply.github.com>
2019-10-09 17:27:51 +0400

    fixed bugs

--- ScrollEvent.js
+++ ScrollEvent.js
@@ -2,31 +2,41 @@
 * @license Copyright (c) 2019, Abo Hovhannisyan. All rights reserved.
 */
 class ScrollEvent{
-
-  on(selector, callback){
-  
-  let elem = document.querySelector(selector);
-
+/*
+  @selector GET 'up', 'down'
+  @callback GET function
+  */
+	on(selector, callback){
+  	let elem = document.querySelector(selector);
+		let key  = 0;
+    let verify = 0;
     document.addEventListener('wheel', function(event){
-    
       let posY = window.scrollY;
       let elem_posY = elem.offsetTop;
 
       let scrollTop = posY + (window.innerHeight - elem.offsetHeight);
-
+         
       if(scrollTop >= elem_posY && 
-      	 scrollTop <= elem_posY + window.innerHeight){
-       	   callback();
+      	scrollTop <= elem_posY + window.innerHeight){
+      	
+        if(key == verify){
+          callback();
+          key++;
+        }
+        
+      }else{
+      	verify = key;
       }
 
     });
-    
   }
-  
+  /*
+  @direction GET 'up', 'down'
+  @callback GET function
+  */
   direction(direction, callback){
   	
-  document.addEventListener('wheel', function(event){
-    
+  	document.addEventListener('wheel', function(event){
     let dir = (event.deltaY > 0)? 'down' : "up";
 		
       if(dir == direction){
